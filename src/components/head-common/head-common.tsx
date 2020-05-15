0e450f0a2a2669738a8d204fda8f94c20935dc73
mathildebuenerd <m.buenerd@lokalportal.de>
2020-05-15 22:29:32 +0200

    Add metatags

--- src/components/head-common/head-common.tsx
+++ src/components/head-common/head-common.tsx
@@ -1,8 +1,33 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
+const title = "Mathilde Buenerd - Front-end Developer in Hamburg";
+const description =
+  "Frontend user interface developer based in Hamburg focusing on web development with modern frontend technologies and practices.";
+
 const HeadCommon: React.FC = () => (
   <Helmet>
+    <html lang="en" />
+
+    <title>{title}</title>
+    <meta name="description" content={description} />
+    {/*Open Graph*/}
+    <meta property="og:site_name" content={title} />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    {/*<meta property="og:url" content="" />*/}
+    <meta property="og:image" content="" />
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:type" content="website" />
+    {/*Twitter cards*/}
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@mathildebuenerd" />
+    <meta name="twitter:creator" content="@mathildebuenerd" />
+    {/*<meta property="og:url" content="" />*/}
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    {/*<meta property="og:image" content="" />*/}
+
     <link
       href="https://fonts.googleapis.com/css?family=Inter:300,400,500,700&display=swap"
       rel="stylesheet"
