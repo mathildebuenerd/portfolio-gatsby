d8bb966dee7954a17d491afaf57ae6dd0966eaed
mathildebuenerd <m.buenerd@lokalportal.de>
2020-05-22 22:52:38 +0200

    Add ogImage

--- src/components/head-common/head-common.tsx
+++ src/components/head-common/head-common.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
+import ogImage from "src/components/who-i-am/illustrations/main-illustration.png";
+
 const title = "Mathilde Buenerd - Front-end Developer in Hamburg";
 const description =
   "Frontend user interface developer based in Hamburg focusing on web development with modern frontend technologies and practices.";
@@ -15,18 +17,14 @@ const HeadCommon: React.FC = () => (
     <meta property="og:site_name" content={title} />
     <meta property="og:title" content={title} />
     <meta property="og:description" content={description} />
-    {/*<meta property="og:url" content="" />*/}
-    <meta property="og:image" content="" />
+    <meta property="og:url" content="mathildebuenerd.dev" />
+    <meta property="og:image" content={ogImage} />
     <meta property="og:image:type" content="image/jpeg" />
     <meta property="og:type" content="website" />
     {/*Twitter cards*/}
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:site" content="@mathildebuenerd" />
     <meta name="twitter:creator" content="@mathildebuenerd" />
-    {/*<meta property="og:url" content="" />*/}
-    <meta property="og:title" content={title} />
-    <meta property="og:description" content={description} />
-    {/*<meta property="og:image" content="" />*/}
   </Helmet>
 );
 
